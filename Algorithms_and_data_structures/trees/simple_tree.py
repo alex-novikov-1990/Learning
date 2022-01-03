bea3c9eab85337a72bb05d1e7b0c39e71f811608
Alexander Novikov <alex.novikov.1990@gmail.com>
2022-01-04 00:36:24 +0300

    Add a simple tree example

---
+++ Algorithms_and_data_structures/trees/simple_tree.py
@@ -0,0 +1,126 @@
+"""A simple example of tree"""
+
+class SimpleTreeNode:
+    """The node of SimpleTree"""
+
+    def __init__(self, val, parent):
+        self.NodeValue = val
+        self.Parent = parent
+        self.Children = []
+
+class SimpleTree:
+    """Simple tree"""
+
+    def __init__(self, root):
+        """The root could be None"""
+        self.Root = root
+
+    def __should_contain(self, node, arg_name):
+        if self.Root is None:
+            if node is None:
+                return
+            raise ValueError(f"{arg_name} doesn't belong to this tree")
+
+        root = node
+        while root.Parent is not None:
+            root = root.Parent
+
+        if self.Root != root:
+            raise ValueError(f"{arg_name} doesn't belong to this tree")
+
+
+    def AddChild(self, ParentNode, NewChild):
+        if NewChild.Parent is not None:
+            raise ValueError(
+                "It is a tree, not a graph (new child has a parent already)"
+            )
+
+        if self.Root is None and ParentNode is None:
+            self.Root = NewChild
+            return
+
+        self.__should_contain(ParentNode, "ParentNode")
+        ParentNode.Children.append(NewChild)
+        NewChild.Parent = ParentNode
+
+    def DeleteNode(self, NodeToDelete):
+        self.__should_contain(NodeToDelete, "NodeToDelete")
+        if self.Root == NodeToDelete:
+            self.Root = None
+            return
+
+        NodeToDelete.Parent.Children.remove(NodeToDelete)
+        NodeToDelete.Parent = None
+
+    def GetAllNodes(self):
+        if self.Root is None:
+            return []
+
+        # depth first
+        def collect(node):
+            result = [node]
+            result.extend([
+                node
+                for child in node.Children
+                for node in collect(child)
+            ])
+            return result
+
+        return collect(self.Root)
+
+    def FindNodesByValue(self, val):
+        if self.Root is None:
+            return []
+
+        # depth first
+        def find(node):
+            result = [node] if node.NodeValue == val else []
+            result.extend([
+                node
+                for child in node.Children
+                for node in find(child)
+                if node.NodeValue == val
+            ])
+            return result
+
+        return find(self.Root)
+
+    def MoveNode(self, OriginalNode, NewParent):
+        if OriginalNode == self.Root:
+            raise ValueError("Can't move root node")
+
+        # fail fast, save the tree
+        self.__should_contain(NewParent, "NewParent")
+
+        self.DeleteNode(OriginalNode)
+        self.AddChild(NewParent, OriginalNode)
+
+    def Count(self):
+        if self.Root is None:
+            return 0
+
+        def count(node):
+            return 1 + sum(map(count, node.Children))
+
+        return count(self.Root)
+
+    def LeafCount(self):
+        if self.Root is None:
+            return 0
+
+        def count_leaves(node):
+            return 1 if len(node.Children) == 0 \
+                else sum(map(count_leaves, node.Children))
+
+        return count_leaves(self.Root)
+
+    def fill_node_levels(self):
+        if self.Root is None:
+            return
+
+        def fill_levels(node, level=0):
+            node.level = level
+            for child in node.Children:
+                fill_levels(child, level+1)
+
+        fill_levels(self.Root)
---
+++ Algorithms_and_data_structures/trees/simple_tree_test.py
@@ -0,0 +1,134 @@
+"""Tests for SimpleTree."""
+
+from simple_tree import SimpleTree, SimpleTreeNode
+
+
+def test_tree_building_and_counts():
+    node1 = SimpleTreeNode(1, None)
+    node2 = SimpleTreeNode(2, None)
+    node3 = SimpleTreeNode(3, None)
+    node4 = SimpleTreeNode(4, None)
+
+    tree = SimpleTree(None)
+    assert tree.Root is None
+    assert tree.Count() == 0
+    assert tree.LeafCount() == 0
+
+    tree.AddChild(None, node1)
+    assert tree.Root == node1
+    assert not node1.Children
+    assert node1.Parent is None
+    assert tree.Count() == 1
+    assert tree.LeafCount() == 1
+
+    tree.AddChild(tree.Root, node2)
+    assert tree.Root == node1
+    assert node1.Children == [node2]
+    assert node1.Parent is None
+    assert node2.Parent == node1
+    assert not node2.Children
+    assert tree.Count() == 2
+    assert tree.LeafCount() == 1
+
+    tree.AddChild(tree.Root, node3)
+    assert node1.Children == [node2, node3]
+    assert node3.Parent == node1
+    assert tree.Count() == 3
+    assert tree.LeafCount() == 2
+
+    tree.AddChild(node2, node4)
+    assert len(node2.Children) == 1
+    assert node4 in node2.Children
+    assert node4.Parent == node2
+    assert tree.Count() == 4
+    assert tree.LeafCount() == 2
+
+def test_tree_node_removal():
+    tree = SimpleTree(None)
+    node1 = SimpleTreeNode(1, None)
+    node2 = SimpleTreeNode(2, None)
+    node3 = SimpleTreeNode(3, None)
+    node4 = SimpleTreeNode(4, None)
+    tree.AddChild(None, node1)
+    tree.AddChild(tree.Root, node2)
+    tree.AddChild(tree.Root, node3)
+    tree.AddChild(node2, node4)
+    assert tree.Count() == 4 # asume
+    assert tree.LeafCount() == 2 # asume
+
+    tree.DeleteNode(node2)
+    assert tree.Count() == 2
+    assert tree.LeafCount() == 1
+    assert node2.Parent is None
+    assert node1.Children == [node3]
+
+def test_tree_node_collection():
+    tree = SimpleTree(None)
+    node1 = SimpleTreeNode(1, None)
+    node2 = SimpleTreeNode(2, None)
+    node3 = SimpleTreeNode(3, None)
+    node4 = SimpleTreeNode(4, None)
+    tree.AddChild(None, node1)
+    tree.AddChild(tree.Root, node2)
+    tree.AddChild(tree.Root, node3)
+    tree.AddChild(node2, node4)
+
+    assert set(tree.GetAllNodes()) == set([node1, node2, node3, node4])
+
+def test_tree_node_move():
+    tree = SimpleTree(None)
+    node1 = SimpleTreeNode(1, None)
+    node2 = SimpleTreeNode(2, None)
+    node3 = SimpleTreeNode(3, None)
+    node4 = SimpleTreeNode(4, None)
+    tree.AddChild(None, node1)
+    tree.AddChild(tree.Root, node2)
+    tree.AddChild(tree.Root, node3)
+    tree.AddChild(node2, node4)
+    assert tree.Count() == 4 # asume
+    assert tree.LeafCount() == 2 # asume
+
+    tree.MoveNode(node2, node3)
+    assert tree.Count() == 4
+    assert tree.LeafCount() == 1
+    assert node2.Parent == node3
+    assert node3.Children == [node2]
+
+def test_tree_node_find():
+    tree = SimpleTree(None)
+    node1 = SimpleTreeNode(1, None)
+    node2 = SimpleTreeNode(2, None)
+    node3 = SimpleTreeNode(1, None)
+    node4 = SimpleTreeNode(2, None)
+    node5 = SimpleTreeNode(1, None)
+    tree.AddChild(None, node1)
+    tree.AddChild(tree.Root, node2)
+    tree.AddChild(tree.Root, node3)
+    tree.AddChild(node2, node4)
+    tree.AddChild(node2, node5)
+    assert tree.Count() == 5 # asume
+    assert tree.LeafCount() == 3 # asume
+
+    assert set(tree.FindNodesByValue(1)) == set([node1, node3, node5])
+
+def test_tree_node_levels():
+    tree = SimpleTree(None)
+    node1 = SimpleTreeNode(1, None)
+    node2 = SimpleTreeNode(2, None)
+    node3 = SimpleTreeNode(1, None)
+    node4 = SimpleTreeNode(2, None)
+    node5 = SimpleTreeNode(1, None)
+    tree.AddChild(None, node1)
+    tree.AddChild(tree.Root, node2)
+    tree.AddChild(tree.Root, node3)
+    tree.AddChild(node2, node4)
+    tree.AddChild(node4, node5)
+    assert tree.Count() == 5 # asume
+    assert tree.LeafCount() == 2 # asume
+
+    tree.fill_node_levels()
+    assert node1.level == 0
+    assert node2.level == 1
+    assert node3.level == 1
+    assert node4.level == 2
+    assert node5.level == 3

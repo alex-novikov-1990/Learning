a7319241b942fcd93be75123575c0ded9fcef3e0
Alexander Novikov <alex.novikov.1990@gmail.com>
2022-06-08 22:31:49 +0300

    Add 12.fs

---
+++ FSharp/12.fs
@@ -0,0 +1,17 @@
+let buildList head tail step =
+    let cmp = if step > 0 then (>=) else (<=)
+    let rec loop list i =
+        if cmp i head then
+            loop (i :: list) (i - step)
+        else
+            list
+    loop [] tail
+
+// 34.1
+let upto n = buildList 1 n 1 // [ 1..n ]
+
+// 34.2
+let dnto n = buildList n 1 -1 // [n..(-1)..1]
+
+// 34.3
+let rec evenn n = buildList 2 (2 * n) 2 // [ 2..2..(2*n) ]

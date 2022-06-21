929744599f0cf8a04e875f185c2a5789c6295062
Alexander Novikov <alex.novikov.1990@gmail.com>
2022-06-21 23:40:03 +0300

    Replace refs with mutables in 18.fs

--- FSharp/18.fs
+++ FSharp/18.fs
@@ -3,12 +3,12 @@ let f n = // imperative
     if n < 0 then
         failwith "n should not be less than 0"
 
-    let i = ref 1
-    let r = ref 1
-    while ! i <= n do
-        r := ! r * ! i
-        i := ! i + 1
-    ! r
+    let mutable i = 1
+    let mutable r = 1
+    while i <= n do
+        r <- r * i
+        i <- i + 1
+    r
 
 // 47.4.2
 let fibo n = // imperative
@@ -17,12 +17,12 @@ let fibo n = // imperative
     elif n = 0 then
         0
     else
-        let i = ref 1
-        let prev = ref 0
-        let result = ref 1
-        while ! i < n do
-            let prev_result = ! result
-            result := ! result + ! prev
-            prev := prev_result
-            i := ! i + 1
-        ! result
+        let mutable i = 1
+        let mutable prev = 0
+        let mutable result = 1
+        while i < n do
+            let prev_result = result
+            result <- result + prev
+            prev <- prev_result
+            i <- i + 1
+        result
